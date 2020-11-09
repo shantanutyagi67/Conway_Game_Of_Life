f6f064df072514e64a702f86b24a512c91d1580a
Shantanu Tyagi <shantanutyagi67@gmail.com>
2020-11-10 01:32:47 +0530

    30

--- src/Rule/rule30.java
+++ src/Rule/rule30.java
@@ -1,8 +1,11 @@
+package Rule;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.RenderingHints;
+import java.awt.Toolkit;
 import java.awt.geom.Rectangle2D;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -20,12 +23,12 @@ public class rule30 extends JComponent implements Runnable{
 	 */
 	private static final long serialVersionUID = 1L;
 	static JFrame frame = new JFrame("%window name%");
-	static int size = 5;
-	double spacing = 0.2;
-	static int width = 1600, height = 900;
+	static int size = 1;
+	double spacing = 0;
+	static int width = 1800, height = 900;
 	static int endState[][] = new int [height/size][width/size];
 	static int h = height/size, w = width/size;
-	static double itr = 1;
+	static int itr = 0;
 	
 	public static void main(String args[]) {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -34,14 +37,15 @@ public class rule30 extends JComponent implements Runnable{
 		frame.getContentPane().setBackground(Color.WHITE);
 		frame.setVisible(true);
 		
-		for(int i=0;i<w;i++) {
-			int tmp = new Random().nextInt(100);
-			if (tmp<50)
-				endState[0][i] = 1;
-			else
-				endState[0][i] = 0;
-			
-		}
+//		for(int i=0;i<w;i++) {
+//			int tmp = new Random().nextInt(100);
+//			if (tmp<50)
+//				endState[0][i] = 1;
+//			else
+//				endState[0][i] = 0;
+//			
+//		}
+		endState[0][w/2] = 1;
 }
 	
 	public void paint(Graphics g)
@@ -57,10 +61,11 @@ public class rule30 extends JComponent implements Runnable{
 				if(endState[i][j]==1) {
 					g2D.setColor(Color.BLACK);
 					sum++;
+					g2D.fill(new Rectangle2D.Double(spacing+j*size, spacing+(i+1)*size, size-2*spacing, size-2*spacing));
+					continue;
 				}
 				else
 					g2D.setColor(Color.WHITE);
-				g2D.fill(new Rectangle2D.Double(spacing+j*size, spacing+(i+1)*size, size-2*spacing, size-2*spacing));
 			}
 		}
 		
@@ -73,30 +78,37 @@ public class rule30 extends JComponent implements Runnable{
 		BigDecimal bd = new BigDecimal(Double.toString(sum/(w*h)*100.00));
 	    bd = bd.setScale(2, RoundingMode.HALF_UP);
 		g2D.drawString("Alive: "+bd.doubleValue()+"%",1200,65);
-		itr++;
 		
-		int[][] next = new int [height/size][width/size];
-		for(int i=0;i<h-1;i++) {
-			for(int j=0;j<w-2;j++) {
-				next[i][j]=endState[i][j];
-				if(endState[i][j]==1&&endState[i][j+1]==1&&endState[i][j+2]==1) next[i+1][j+1]=0;
-				else if(endState[i][j]==1&&endState[i][j+1]==1&&endState[i][j+2]==0) next[i+1][j+1]=0;
-				else if(endState[i][j]==1&&endState[i][j+1]==0&&endState[i][j+2]==1) next[i+1][j+1]=0;
-				else if(endState[i][j]==1&&endState[i][j+1]==0&&endState[i][j+2]==0) next[i+1][j+1]=1;
-				else if(endState[i][j]==0&&endState[i][j+1]==1&&endState[i][j+2]==1) next[i+1][j+1]=1;
-				else if(endState[i][j]==0&&endState[i][j+1]==1&&endState[i][j+2]==0) next[i+1][j+1]=1;
-				else if(endState[i][j]==0&&endState[i][j+1]==0&&endState[i][j+2]==1) next[i+1][j+1]=1;
-				else if(endState[i][j]==0&&endState[i][j+1]==0&&endState[i][j+2]==0) next[i+1][j+1]=0;
-				endState[i][j] = next[i][j];
-			}
+		int[] next = new int [w];
+		for(int j=0;j<w;j++) {
+			next[j] = endState[itr][j];
 		}
-		
-		//for(int i=0;i<h;i++) {
-			//for(int j=0;j<w;j++) {
-				//endState[i][j] = next[i][j];
-			//}
-		//}
-		
+		int i = itr;
+		for(int j=0;j<w-2;j++) {
+			if(endState[i][j]==1&&endState[i][j+1]==1&&endState[i][j+2]==1) next[j+1]=0;
+			else if(endState[i][j]==1&&endState[i][j+1]==1&&endState[i][j+2]==0) next[j+1]=0;
+			else if(endState[i][j]==1&&endState[i][j+1]==0&&endState[i][j+2]==1) next[j+1]=0;
+			else if(endState[i][j]==1&&endState[i][j+1]==0&&endState[i][j+2]==0) next[j+1]=1;
+			else if(endState[i][j]==0&&endState[i][j+1]==1&&endState[i][j+2]==1) next[j+1]=1;
+			else if(endState[i][j]==0&&endState[i][j+1]==1&&endState[i][j+2]==0) next[j+1]=1;
+			else if(endState[i][j]==0&&endState[i][j+1]==0&&endState[i][j+2]==1) next[j+1]=1;
+			else if(endState[i][j]==0&&endState[i][j+1]==0&&endState[i][j+2]==0) next[j+1]=0;
+		}
+		itr++;
+		if(itr>=900) {
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			};
+		}
+		for(int j=0;j<w;j++) {
+			endState[itr][j] = next[j];
+		}
+		Toolkit t=Toolkit.getDefaultToolkit();  
+        Image im=t.getImage("30.jpg");//.getScaledInstance(200, 100, Image.SCALE_DEFAULT); 
+        g2D.drawImage(im, 3,3,this);
 		run();
 		//repaint();
 	}
@@ -104,7 +116,7 @@ public class rule30 extends JComponent implements Runnable{
 	@Override
 	public void run() {
 		try {
-			Thread.sleep(20);
+			Thread.sleep(0);
 			repaint();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
